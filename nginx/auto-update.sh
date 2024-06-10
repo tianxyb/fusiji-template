e8c968ecc2b4e47d4fe5bd47f7a234f46d8c7674
xyb tian <tianxyb@xybdeMacBook-Pro.local>
2024-06-10 16:51:47 +0800

    support multi users

--- nginx/auto-update.sh
+++ nginx/auto-update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 NGINX_CONF_PATH=/etc/nginx/conf.d
-NGINX_FILE=fusiji.tk.conf
+NGINX_FILE=fusiji.site.conf
 
-RMOTE_NGINX_CONF_URL=https://raw.githubusercontent.com/tianxyb/fusiji-template/master/nginx/fusiji.tk.conf
+RMOTE_NGINX_CONF_URL=https://raw.githubusercontent.com/tianxyb/fusiji-template/master/nginx/fusiji.site.conf
 
 log()
 {
@@ -20,37 +20,38 @@ error()
 	log "[ERROR] $@"
 }
 
-upgrade_file(){
-        local filepath=$1
-        local filename=$2
-        local remote_url=$3
+upgrade_file()
+{
+	local filepath=$1
+	local filename=$2
+	local remote_url=$3
 
 	local reload_cmd=$4
-        mkdir -p $filepath
+	mkdir -p $filepath
 
-        info ">>> download $filename ..." 
-        local http_code=$(curl -L -w "%{http_code}" -o /tmp/tmpfile "$remote_url") 
+	info ">>> download $filename ..."
+	local http_code=$(curl -L -w "%{http_code}" -o /tmp/tmpfile "$remote_url")
 	rtn=$?
-        if [ "$rtn" == 0 -a "$http_code" == 200 ]; then
+	if [ "$rtn" == 0 -a "$http_code" == 200 ]; then
 		local diffresult=
-                if [ -f  $filepath/$filename ]; then
-                	diffresult=$(diff /tmp/tmpfile $filepath/$filename)
-                	[ -z "$diffresult" ] && {
-				info "upgrade success, but the file is already up to date." 
+		if [ -f $filepath/$filename ]; then
+			diffresult=$(diff /tmp/tmpfile $filepath/$filename)
+			[ -z "$diffresult" ] && {
+				info "upgrade success, but the file is already up to date."
 				return 0
 			}
 		fi
 
-        	[ -f $filepath/$filename ] && {
-                	info ">>> backup $filename first ..."
-                	cp $filepath/$filename $filepath/$filename.bk
-        	}   
+		[ -f $filepath/$filename ] && {
+			info ">>> backup $filename first ..."
+			cp $filepath/$filename $filepath/$filename.bk
+		}
 
-               	mv /tmp/tmpfile $filepath/$filename; 
+		mv /tmp/tmpfile $filepath/$filename
 		error "*** upgrade success!"
 		[ "$diffresult" ] && {
-			info "file content diff(new old):"; 
-			info "$diffresult"; 
+			info "file content diff(new old):"
+			info "$diffresult"
 		}
 
 		[ "$reload_cmd" ] && {
@@ -58,16 +59,17 @@ upgrade_file(){
 			rtn=$?
 			info "reload [$reload_cmd], rtn=$rtn"
 		}
-                return 0
-	else  
-		error "***Error: download file error[$http_code]: $(cat /tmp/tmpfile) "; 
-		rm -f /tmp/tmpfile; 
-		return 1; 
+		return 0
+	else
+		error "***Error: download file error[$http_code]: $(cat /tmp/tmpfile) "
+		rm -f /tmp/tmpfile
+		return 1
 	fi
 }
 
-upgrade_proxy_conf(){
-        upgrade_file "$NGINX_CONF_PATH" "$NGINX_FILE" "$RMOTE_NGINX_CONF_URL" "systemctl restart nginx"
+upgrade_proxy_conf()
+{
+	upgrade_file "$NGINX_CONF_PATH" "$NGINX_FILE" "$RMOTE_NGINX_CONF_URL" "systemctl restart nginx"
 }
 
 upgrade_proxy_conf
